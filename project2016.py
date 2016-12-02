b82a1694c211459493f8570475a1c5de8a503870
shiva80 <shivamehtari@gmail.com>
2016-12-01 23:15:22 -0800

    Add files via upload

---
+++ project2016.py
@@ -0,0 +1,7 @@
+a=input('Enter first number')
+b=input('Enter second number')
+c=input('Enter third number')
+Addition=a+b+c
+multiplication=a*b*c
+print('Addition=',Addition)
+print('multiplication=',multiplication)
